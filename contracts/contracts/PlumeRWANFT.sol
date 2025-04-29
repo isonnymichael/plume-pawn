eff898a033b2b8d6ba5d9c034f4590b6080ee898
Sonny Michael <isonnymichael@gmail.com>
2025-04-29 21:30:46 +0700

    feat: update plume rwa nft

--- contracts/contracts/PlumeRWANFT.sol
+++ contracts/contracts/PlumeRWANFT.sol
@@ -1,29 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract PlumeRWANFT is ERC1155, Ownable {
+    using Strings for uint256;
+    
     uint256 public nextTokenId = 1;
 
     struct AssetInfo {
         string name;
         string ticker;
-        uint256 supply;
+        uint256 maxSupply;
+        uint256 currentSupply;
         string metadataURI;
     }
 
     mapping(uint256 => AssetInfo) public assetInfo;
+    mapping(uint256 => address) public tokenCreators;
 
-    constructor() ERC1155("") {}
+    event AssetMinted(
+        uint256 indexed tokenId,
+        string name,
+        string ticker,
+        uint256 amount,
+        string metadataURI,
+        address creator
+    );
+
+    constructor() ERC1155("") Ownable(msg.sender) {}
 
     function mintRWA(
         string memory name,
         string memory ticker,
         uint256 amount,
         string memory metadataURI
-    ) external onlyOwner {
+    ) external onlyOwner returns (uint256) {
+        require(amount > 0, "Amount must be greater than 0");
+        require(bytes(name).length > 0, "Name cannot be empty");
+        require(bytes(ticker).length > 0, "Ticker cannot be empty");
+        require(bytes(metadataURI).length > 0, "Metadata URI cannot be empty");
+
         uint256 tokenId = nextTokenId++;
 
         _mint(msg.sender, tokenId, amount, "");
@@ -31,14 +50,30 @@ contract PlumeRWANFT is ERC1155, Ownable {
         assetInfo[tokenId] = AssetInfo({
             name: name,
             ticker: ticker,
-            supply: amount,
+            maxSupply: amount,
+            currentSupply: amount,
             metadataURI: metadataURI
         });
+        
+        tokenCreators[tokenId] = msg.sender;
 
-        _setURI(metadataURI);
+        emit AssetMinted(tokenId, name, ticker, amount, metadataURI, msg.sender);
+        
+        return tokenId;
     }
 
     function uri(uint256 tokenId) public view override returns (string memory) {
+        require(tokenId > 0 && tokenId < nextTokenId, "Token does not exist");
         return assetInfo[tokenId].metadataURI;
+    }
+
+    function getAssetInfo(uint256 tokenId) public view returns (AssetInfo memory) {
+        require(tokenId > 0 && tokenId < nextTokenId, "Token does not exist");
+        return assetInfo[tokenId];
+    }
+
+    function getTokenCreator(uint256 tokenId) public view returns (address) {
+        require(tokenId > 0 && tokenId < nextTokenId, "Token does not exist");
+        return tokenCreators[tokenId];
     }
 }
